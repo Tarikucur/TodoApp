8db856038280c40ec34ac1fdbcf2112225b98d31
Ahmet Tarık Uçur <91345134+Tarikucur@users.noreply.github.com>
2024-01-09 20:59:30 +0300

    Spring Boot

---
+++ src/test/java/com/todotasks/springtodoappdemo/SpringTodoappDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todotasks.springtodoappdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTodoappDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
