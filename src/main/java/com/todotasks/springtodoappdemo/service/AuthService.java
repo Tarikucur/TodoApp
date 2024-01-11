39af7dbbd7254acfb20aee6ec6d0420e859ee8eb
Ahmet Tarık Uçur <av.atarikucur@gmail.com>
2024-01-11 13:46:55 +0300

    BCrypt password encryption implemented

--- src/main/java/com/todotasks/springtodoappdemo/service/AuthService.java
+++ src/main/java/com/todotasks/springtodoappdemo/service/AuthService.java
@@ -4,22 +4,26 @@ import com.todotasks.springtodoappdemo.controller.Auth.AuthenticationRequestBody
 import com.todotasks.springtodoappdemo.controller.Auth.AuthenticationResponse;
 import com.todotasks.springtodoappdemo.model.UserEntity;
 import com.todotasks.springtodoappdemo.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthService {
     private final TokenService tokenService;
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public AuthService(TokenService tokenService,
-                       UserRepository userRepository) {
+                       UserRepository userRepository,
+                       PasswordEncoder passwordEncoder) {
         this.tokenService = tokenService;
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public AuthenticationResponse authenticate(AuthenticationRequestBody request){
         UserEntity userEntity = userRepository.findByEmail(request.email);
-        if(userEntity != null && userEntity.getPassword().equals(request.password)){
+        if(userEntity != null && passwordEncoder.matches(request.password, userEntity.getPassword())){
             return new AuthenticationResponse(tokenService.generateToken(userEntity.getEmail()),
                     userEntity.getName(), userEntity.getEmail(), userEntity.getId());
         }
@@ -28,6 +32,8 @@ public class AuthService {
 
     public boolean register(UserEntity userEntity) {
         try {
+            String encodedPassword = passwordEncoder.encode(userEntity.getPassword());
+            userEntity.setPassword(encodedPassword);
             userRepository.save(userEntity);
             return true;
         } catch(Exception e) {
--- src/main/java/com/todotasks/springtodoappdemo/service/TokenService.java
+++ src/main/java/com/todotasks/springtodoappdemo/service/TokenService.java
@@ -25,9 +25,6 @@ public class TokenService {
     }
 
     private String createToken(Map<String, Object> claims, String userName) {
-        System.out.println("1111111111111111111111111111111111111111");
-        System.out.println(Jwts.claims());
-        System.out.println("2222222222222222222222222222222222222222");
         return Jwts.builder()
                 .setClaims(claims)
                 .setSubject(userName)
